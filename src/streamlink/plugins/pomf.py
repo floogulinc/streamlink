a4ef1d94c96231433251f641d09831ff096e0df3
Paul Friederichsen <floogulinc@gmail.com>
2021-09-15 19:10:43 -0500

    add temp 720p option

--- src/streamlink/plugins/pomf.py
+++ src/streamlink/plugins/pomf.py
@@ -4,6 +4,8 @@ import re
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import useragents
 from streamlink.stream import HTTPStream
+from streamlink.stream import HLSStream
+
 
 log = logging.getLogger(__name__)
 
@@ -18,6 +20,8 @@ class Pomf(Plugin):
         self.session.http.headers.update({'User-Agent': useragents.CHROME, 'Referer': self.url})
         flv_url= "https://server2.pomf.tv/srs/live/{}.flv".format(channel)
         yield "live", HTTPStream(self.session, flv_url)
+        p720_url = "https://server2.pomf.tv/hls-live/{}_hd720/index.m3u8".format(channel)
+        yield "720p", HLSStream(self.session, p720_url)
 
 
 __plugin__ = Pomf
